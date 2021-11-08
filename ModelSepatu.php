38579d0aeeb7a2576f38c0cb619bafb0ab7fed09
yogitaufik <90616708+yogitaufik@users.noreply.github.com>
2021-11-08 09:41:28 +0700

    Create ModelSepatu.php

---
+++ ModelSepatu.php
@@ -0,0 +1,26 @@
+<?php
+
+class ModelSepatu extends CI_Model
+{
+        	public $harga;
+        	public function process($data)
+        	{
+	              	if($data = "Nike"){
+			            $this->harga = 375000;
+		              }
+	              	elseif($data ="Adidas"){
+	              	$this->harga = 300000;
+                  }
+              		elseif($data ="Kickers"){
+		            	$this->harga = 250000;
+	              	}
+               		elseif($data ="Eiger"){
+	            		$this->harga = 275000;
+		              }
+              		else{
+	             		$this->harga = 400000;
+		              }
+
+	      return $this->harga;
+      	}
+}
